77e4b04078503306cfb39c44e3a1812dd9f77dcf
Sof08 <smeziani8@hotmail.fr>
2023-08-13 23:32:17 +0200

    Initialisation
    - TODO : fermer la modale
    - Implémenter entrées du formulaire
    - Ajouter validation ou messages d'erreur

---
+++ starterOnly/modal.js
@@ -0,0 +1,120 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+//const => fermer la fenetre 
+const modalClose = document.getElementsByClassName("close");
+const form = document.getElementById("form");
+// Liaison des labels
+const prenom = document.getElementById("first");
+const prenom_erreur = document.getElementById("first_error");
+const nom =  document.getElementById("last");
+const nom_erreur =document.getElementById("last_error");
+const email =  document.getElementById("email");
+const email_erreur = document.getElementById("email_error");
+const nb_concours = document.getElementById("quantity");
+const concours_erreur = document.getElementById("quantity_error");
+const locationList = document.querySelectorAll('input[name="location"]');
+const location_erreur = document.getElementById("location_error");
+const checkbox1 = document.getElementById("checkbox1");
+const checkbox2 = document.getElementById("checkbox2");
+const condition_erreur = document.getElementById("condition_error");
+
+const submitBtn = document.querySelector(".btn-submit");
+
+
+
+//List regex
+var mailRegex = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/;
+var nomRegex = /^[a-zA-Zéèîï][a-zéèêàçîï]+([-'\s][a-zA-Zéèîï][a-zéèêàçîï]+)?/;
+var concoursRegex =/^[0-9][0-9]?$|^999$/;
+var naissanceRegex = /^([0-9]{2})|([0-9]{2})|([0-9]{4})$/;
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event=> Fermer Form class "bground"
+modalClose[0].addEventListener ("click", closeModal);
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+
+//fermer la modale
+function closeModal() {
+  modalbg.style.display = "none";
+};
+
+//Bouton valider pour envoyer le formulaire
+submitBtn.addEventListener("click",validate); 
+
+function validate () {
+
+  //Affichage du message d'erreur si le champs Prénom n'est pas vide ou < a 2 caractères : retourne true 
+  if (prenom.value.trim() == '' || prenom.value.length < 2){
+    prenom_erreur.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+  }else{ //validation champs prenom
+    prenom_erreur.innerHTML  = "";
+  }
+  
+  //Affichage du message d'erreur si le champs NOM n'est pas vide ou < a 2 caractères : retourne true 
+  if (nom.value.trim() == '' || nom.value.length <2){
+    nom_erreur.innerHTML  = "Veuillez entrer 2 caractères ou plus pour le champ du nom."; 
+  }else{
+    nom_erreur.innerHTML  = "";
+  }
+  // Utiliser mailRegex afin de vérifier l'@mail
+  if(mailRegex.test(email.value)== false){
+    email_erreur.innerHTML  ="Veuillez entrer un email correct";
+  }
+  else{
+    email_erreur.innerHTML  = "";
+  }
+
+  // Pour le nombre de concours, une valeur numérique est saisie || non vide 
+  if (nb_concours.value == '' || concoursRegex.test(nb_concours.value) == false){
+    concours_erreur.innerHTML  = "Veuillez entrer le nombre de tournois participé";
+  }else{
+    concours_erreur.innerHTML  = "";
+  }
+
+
+
+  // verifier si une localisation est sélectionnée.
+  let location = ""
+  for (let i = 0; i < locationList.length; i++) {
+    if (locationList[i].checked) {
+      location = locationList[i].value;
+      return true;
+    } else {
+      location_erreur.innerHTML = "Vous devez choisir une option";
+    }
+  }
+
+  // vérifier si les conditions sont cochées 
+  if(checkbox1.checked){
+    condition_erreur.innerHTML  = "";
+  }
+  else{
+    condition_erreur.innerHTML  ="Vous devez vérifier que vous acceptez les termes et conditions";
+  }
+
+ 
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+  })
+  
+}
+
+
